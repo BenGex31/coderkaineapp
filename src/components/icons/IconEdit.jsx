5a34d896228b48eea64e68ab06eea56c99d00613
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-21 11:02:43 +0200

    Create IconEdit.jsx

---
+++ src/components/icons/IconEdit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import colors from '../../../utils/style/colors';
+
+const IconEdit = () => {
+  return <EditIcon color={colors.darkBlue} sx={{ width: 15, height: 15 }} />;
+};
+
+export default IconEdit;
