6a161ee75737ad0540e3baca2081dd2a7b3721b1
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-21 11:02:39 +0200

    Create IconClose.jsx

---
+++ src/components/icons/IconClose.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import colors from '../../../utils/style/colors';
+
+const IconClose = () => {
+  return (
+    <CloseIcon color={colors.lightBlue} sx={{ width: 15.38, height: 15.38 }} />
+  );
+};
+
+export default IconClose;
