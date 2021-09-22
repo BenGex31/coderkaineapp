349ce83694bdde5a57778cb93b8c53135c8e9a74
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-22 16:46:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+import { hasAuthenticated } from './utils/API/AuthApi';
+import Auth from './context/Auth';
+import AutenticatedRoute from './components/AuthenticatedRoute/AutenticatedRoute';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
+    <Auth.Provider value={{isAuthenticated, setIsAuthenticated}}>
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <AutenticatedRoute exact path="/home" component={Home} />
+        </Switch>
+      </Router>
+    </Auth.Provider>
   );
 }
 
