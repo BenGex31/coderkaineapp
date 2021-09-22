9daddfe608fd36fe7e770ff816c9451bf7e41870
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-22 17:22:44 +0200

    Update AuthApi.js

--- src/utils/API/AuthApi.js
+++ src/utils/API/AuthApi.js
@@ -4,7 +4,7 @@ import { getItem, addItem, removeItem } from '../LocalStorage/LocalStorage';
 
 export function hasAuthenticated() {
   const token = getItem('coderkaineToken');
-  const result = token ? tokenIsValid(token) : false;
+  const result = token ? true : false;
 
   if (false === result) {
     removeItem('coderkaineToken');
@@ -14,7 +14,7 @@ export function hasAuthenticated() {
 
 export function login(email, password) {
   return axios
-    .post('https://api-pp.hifivework.com/apiv1/auth/login', email, password)
+    .post('https://api-pp.hifivework.com/apiv1/auth/login', {login: email, password: password})
     .then((response) => response.data)
     .then((token) => {
       addItem('coderkaineToken', token);
@@ -27,10 +27,10 @@ export function logout() {
   removeItem('coderkaineToken');
 }
 
-function tokenIsValid(token) {
+/*function tokenIsValid(token) {
   const { exp } = jwtDecode(token);
   if (exp * 1000 > new Date().getTime()) {
     return true;
   }
   return false;
-}
+}*/
