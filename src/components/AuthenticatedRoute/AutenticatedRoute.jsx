f0c7c3472e87725b5d52d156dc8b5977120e350f
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-22 16:46:35 +0200

    Create AutenticatedRoute.jsx

---
+++ src/components/AuthenticatedRoute/AutenticatedRoute.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import Auth from '../../context/Auth';
+import { Route, Redirect } from 'react-router-dom';
+
+const AutenticatedRoute = ({ path, component }) => {
+  const { isAuthenticated } = useContext(Auth);
+
+  return isAuthenticated ? (
+    <Route exact path={path} component={component} />
+  ) : (
+    <Redirect to="/login" />
+  );
+};
+
+export default AutenticatedRoute;
