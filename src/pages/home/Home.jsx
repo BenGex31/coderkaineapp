fc90d434f3e3a790dd0eaa56d3ec694cb5baa48b
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-23 09:42:41 +0200

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import Auth from '../../context/Auth';
+import colors from '../../utils/style/colors';
+import './Home.css';
 
 const Home = () => {
   const { isAuthenticated } = useContext(Auth);
   return (
-    <div>
+    <div className="containerMainHome">
       <NavBar />
       <h1>Home</h1>
       liste des employés
