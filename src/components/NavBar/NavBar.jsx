27cb1af631dbfd83c5b41a5edbba9a4cd8a9bf66
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-23 09:42:34 +0200

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import Auth from '../../context/Auth';
 import { logout } from '../../utils/API/AuthApi';
+import logoCoderKaine from '../../assets/logo/logo-coderkaine.svg';
+import './NavBar.css';
 
 const NavBar = () => {
   const { isAuthenticated, setIsAuthenticated } = useContext(Auth);
@@ -12,8 +14,18 @@ const NavBar = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleLogOut}>Se déconnecter</button>
+    <div className="containerNavBarHome">
+      <div className="containerNavBarLeft">
+        <img
+          style={{ width: 33, height: 33 }}
+          src={logoCoderKaine}
+          alt="logo CoderKaine"
+        />
+      </div>
+      <div className="containerNavBarRight">
+        <span>Bonjour User</span>
+        <span onClick={handleLogOut}>Se déconnecter</span>
+      </div>
     </div>
   );
 };
