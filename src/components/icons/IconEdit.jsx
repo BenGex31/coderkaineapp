4aaa008d178c7b5af2b141d43e9eddd784998ece
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-24 14:21:30 +0200

    Update IconEdit.jsx

--- src/components/icons/IconEdit.jsx
+++ src/components/icons/IconEdit.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import colors from '../../utils/style/colors';
 
 const IconEdit = () => {
-  return <EditIcon color={colors.darkBlue} sx={{ width: 15, height: 15 }} />;
+  return (
+    <IconButton size="small" color="inherit" aria-label="edit user">
+      <EditIcon sx={{ width: 15, height: 15, color: colors.darkBlue }} />
+    </IconButton>
+  );
 };
 
 export default IconEdit;
