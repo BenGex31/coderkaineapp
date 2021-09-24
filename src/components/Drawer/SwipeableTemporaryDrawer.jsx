f055b7626023c615bc3b18747d926b947a0a89e8
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-24 15:58:41 +0200

    Update SwipeableTemporaryDrawer.jsx

--- src/components/Drawer/SwipeableTemporaryDrawer.jsx
+++ src/components/Drawer/SwipeableTemporaryDrawer.jsx
@@ -1,74 +1,35 @@
 import React from 'react';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import TitleH2 from '../Titles/TitleH2';
 
-const SwipeableTemporaryDrawer = () => {
-  const [state, setState] = React.useState({
-    right: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event &&
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
-  const list = (anchor) => (
+const SwipeableTemporaryDrawer = ({
+  anchor,
+  isOpenDrawer,
+  onCloseDrawer,
+  onOpenDrawer,
+}) => {
+  const list = () => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 400 }}
+      sx={{ width: 404 }}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      //onClick={toggleDrawer(anchor, false)}
+      //onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      <TitleH2 text="Modification d’un employé" />
     </Box>
   );
 
   return (
     <div>
-      {['right'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <SwipeableDrawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-            onOpen={toggleDrawer(anchor, true)}
-          >
-            {list(anchor)}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))}
+      <SwipeableDrawer
+        anchor={anchor}
+        open={isOpenDrawer}
+        onClose={onCloseDrawer}
+        onOpen={onOpenDrawer}
+      >
+        {list()}
+      </SwipeableDrawer>
     </div>
   );
 };
