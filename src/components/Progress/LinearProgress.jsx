6c98c6037c917a322cc7e0592acac23942d4c7d1
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-24 10:08:25 +0200

    Create LinearProgress.jsx

---
+++ src/components/Progress/LinearProgress.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import LinearProgress from '@mui/material/LinearProgress';
+
+export default function LinearColor() {
+  return <LinearProgress color="inherit" />;
+}
