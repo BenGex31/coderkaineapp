daa054c0fa6f386ef9772681d44c92a534d7e4cc
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-27 11:57:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
-//import { hasAuthenticated } from './utils/API/AuthApi';
+import { hasAuthenticated } from './utils/API/AuthApi';
 import Auth from './context/Auth';
 import AutenticatedRoute from './components/AuthenticatedRoute/AutenticatedRoute';
-import AuthUser from './context/AuthUser';
+//import AuthUser from './context/AuthUser';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [apiInfo, setApiInfo] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
+  //const [apiInfo, setApiInfo] = useState();
+  const [currentUser, setCurrentUser] = useState(null);
   return (
-    <Auth.Provider value={{ isAuthenticated, setIsAuthenticated }}>
-      <AuthUser.Provider value={{apiInfo, setApiInfo}}>
+    <Auth.Provider value={{ isAuthenticated, setIsAuthenticated, currentUser, setCurrentUser }}>
+      {/*AuthUser.Provider value={{ apiInfo, setApiInfo }}>*/}
         <Router>
           <Switch>
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Login} />
             <AutenticatedRoute exact path="/home" component={Home} />
           </Switch>
         </Router>
-        </AuthUser.Provider>
+      {/*</AuthUser.Provider>*/}
     </Auth.Provider>
   );
 }
