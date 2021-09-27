92553c99e89f20233e540659411ea6da07fe3978
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-27 16:19:00 +0200

    Update Auth.js

--- src/context/Auth.js
+++ src/context/Auth.js
@@ -4,5 +4,7 @@ export default React.createContext({
   isAuthenticated: false,
   setIsAuthenticated: (value) => {},
   currentUser: null,
-  setCurrentUser: (value) => {}
+  setCurrentUser: (value) => {},
+  employees: null,
+  setEmployees: (value) => {}
 });
