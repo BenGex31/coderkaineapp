492c8f46084f296e2b4ce440e2c05751db446875
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-27 16:18:55 +0200

    Update SwipeableTemporaryDrawer.jsx

--- src/components/Drawer/SwipeableTemporaryDrawer.jsx
+++ src/components/Drawer/SwipeableTemporaryDrawer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Box from '@mui/material/Box';
 import TitleH2 from '../Titles/TitleH2';
@@ -7,76 +7,18 @@ import IconClose from '../icons/IconClose';
 import colors from '../../utils/style/colors';
 import TextFieldInput from '../TextField/TextField';
 import UpdateButton from '../Button/Button';
+import Auth from '../../context/Auth';
 
 const SwipeableTemporaryDrawer = ({
   anchor,
   isOpenDrawer,
   onCloseDrawer,
   onOpenDrawer,
-  onClick,
+  closeDrawer,
+  displayEmployeeLastName,
+  displayEmployeeFirstName,
 }) => {
-  const list = () => (
-    <Box
-      sx={{ width: 404 }}
-      role="presentation"
-      //onClick={toggleDrawer(anchor, false)}
-      //onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <Stack
-        sx={{ paddingBottom: 10 }}
-        direction="row"
-        justifyContent="flex-end"
-      >
-        <IconClose onClick={onClick} />
-      </Stack>
-      <Stack spacing={6} sx={{ paddingLeft: 5, paddingRight: 5 }}>
-        <Stack spacing={1}>
-          <TitleH2 text="Modification d’un employé" />
-          <p
-            style={{
-              color: colors.lightBlue,
-              fontFamily: 'Montserrat, sans-serif',
-              fontWeight: 500,
-              fontSize: 13,
-            }}
-          >
-            Une coquille s’est glissée dans le prénom ou le nom de votre employé
-            ? Vous pouvez le modifier ci-dessous.
-          </p>
-        </Stack>
-        <Stack spacing={3}>
-          <TextFieldInput
-            id="lastName"
-            label="Nom"
-            variant="outlined"
-            defaultValue="userLastName"
-          />
-          <TextFieldInput
-            id="firstName"
-            label="Prénom"
-            variant="outlined"
-            defaultValue="userFirstName"
-          />
-        </Stack>
-        <Stack direction="row" justifyContent="flex-end">
-          <UpdateButton
-            variant="contained"
-            text="Modifier"
-            style={{
-              backgroundColor: colors.darkBlue,
-              color: 'white',
-              width: 180,
-              height: 38,
-              fontSize: 14,
-              fontWeight: 400,
-              borderRadius: 5,
-              fontFamily: 'Montserrat, sans-serif',
-            }}
-          />
-        </Stack>
-      </Stack>
-    </Box>
-  );
+  const { employees } = useContext(Auth);
 
   return (
     <div>
@@ -86,7 +28,67 @@ const SwipeableTemporaryDrawer = ({
         onClose={onCloseDrawer}
         onOpen={onOpenDrawer}
       >
-        {list()}
+        <Box
+          sx={{ width: 404 }}
+          role="presentation"
+          //onClick={toggleDrawer(anchor, false)}
+          //onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <Stack
+            sx={{ paddingBottom: 10 }}
+            direction="row"
+            justifyContent="flex-end"
+          >
+            <IconClose onClick={closeDrawer} />
+          </Stack>
+          <Stack spacing={6} sx={{ paddingLeft: 5, paddingRight: 5 }}>
+            <Stack spacing={1}>
+              <TitleH2 text="Modification d’un employé" />
+              <p
+                style={{
+                  color: colors.lightBlue,
+                  fontFamily: 'Montserrat, sans-serif',
+                  fontWeight: 500,
+                  fontSize: 13,
+                }}
+              >
+                Une coquille s’est glissée dans le prénom ou le nom de votre
+                employé ? Vous pouvez le modifier ci-dessous.
+              </p>
+            </Stack>
+            <Stack spacing={3}>
+              <TextFieldInput
+                id="lastName"
+                label="Nom"
+                variant="outlined"
+                defaultValue="userLastName"
+              />
+              <TextFieldInput
+                id="firstName"
+                label="Prénom"
+                variant="outlined"
+                defaultValue="userFirstName"
+              />
+            </Stack>
+            <Stack direction="row" justifyContent="flex-end">
+              <UpdateButton
+                //onClick={}
+                variant="contained"
+                text="Modifier"
+                style={{
+                  backgroundColor: colors.darkBlue,
+                  color: 'white',
+                  width: 180,
+                  height: 38,
+                  fontSize: 14,
+                  fontWeight: 400,
+                  borderRadius: 5,
+                  fontFamily: 'Montserrat, sans-serif',
+                }}
+              />
+            </Stack>
+          </Stack>
+        </Box>
       </SwipeableDrawer>
     </div>
   );
