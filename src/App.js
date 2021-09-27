969d3cc27db1f0bde5fb5a362b46e602986d8b88
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-27 12:41:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,22 +6,19 @@ import Home from './pages/home/Home';
 import { hasAuthenticated } from './utils/API/AuthApi';
 import Auth from './context/Auth';
 import AutenticatedRoute from './components/AuthenticatedRoute/AutenticatedRoute';
-//import AuthUser from './context/AuthUser';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(hasAuthenticated());
-  //const [apiInfo, setApiInfo] = useState();
   const [currentUser, setCurrentUser] = useState(null);
+  //const [employees, setEmployees] = useState(null)
   return (
     <Auth.Provider value={{ isAuthenticated, setIsAuthenticated, currentUser, setCurrentUser }}>
-      {/*AuthUser.Provider value={{ apiInfo, setApiInfo }}>*/}
         <Router>
           <Switch>
             <Route exact path="/" component={Login} />
             <AutenticatedRoute exact path="/home" component={Home} />
           </Switch>
         </Router>
-      {/*</AuthUser.Provider>*/}
     </Auth.Provider>
   );
 }
