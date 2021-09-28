79b5002d5da8c36443459c0c6f2283120e3a6b02
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-28 15:09:32 +0200

    Create FetchEmployees.js

---
+++ src/utils/API/FetchEmployees.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const fetchEmployees = async (companyId, authToken, setEmployeesList, setEmployees, setDataLoading) => {
+  try {
+    const response = await axios.get(
+      `https://api-pp.hifivework.com/apiv1/company/${companyId}/employees`,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    setEmployeesList(response.data);
+    setEmployees(response.data);
+    setDataLoading(false);
+  } catch (error) {
+    console.error(error);
+  }
+};
