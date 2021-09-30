67f303f05387d791c68e63c0da494082bf7dfad5
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-30 15:16:21 +0200

    Update IconEdit.jsx

--- src/components/icons/IconEdit.jsx
+++ src/components/icons/IconEdit.jsx
@@ -3,7 +3,7 @@ import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import colors from '../../utils/style/colors';
 
-const IconEdit = ({ onClick }) => {
+const IconEdit = ({ onClick, text }) => {
   return (
     <IconButton
       onClick={onClick}
@@ -12,6 +12,7 @@ const IconEdit = ({ onClick }) => {
       aria-label="edit user"
     >
       <EditIcon sx={{ width: 15, height: 15, color: colors.darkBlue }} />
+      <span style={{ fontSize: 13, color: colors.darkBlue }}>{text}</span>
     </IconButton>
   );
 };
