e89b4fb8adb7dc44d3d9e0990487f64c9521c83a
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-30 15:16:25 +0200

    Create MultipleSelect.jsx

---
+++ src/components/Select/MultipleSelect.jsx
@@ -0,0 +1,74 @@
+import React, { useContext } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { EmployeesContext } from '../../context/Employees';
+import colors from '../../utils/style/colors';
+
+export default function MultipleSelectNative({ employees }) {
+  /*const [idEmployee, setIdEmployee] = useState([]);
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');*/
+
+  const { setIdEmployee, setLastName, setFirstName, firstName, lastName } =
+    useContext(EmployeesContext);
+
+  const handleChangeMultiple = (event) => {
+    const { options } = event.target;
+    //const value = [];
+    for (let i = 0, l = options.length; i < l; i += 1) {
+      if (options[i].selected) {
+        //value.push(options[i].value);
+        setIdEmployee(options[i].id);
+        let str = options[i].value;
+        let words = str.split(' ');
+        setLastName(words[0]);
+
+        if (words.length > 2) {
+          setFirstName(words[1] + ' ' + words[2]);
+        } else {
+          setFirstName(words[1]);
+        }
+      }
+    }
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120, maxWidth: 300 }}>
+        <InputLabel
+          sx={{ color: colors.darkBlue }}
+          shrink
+          id="select-employees-label"
+        >
+          Employés
+        </InputLabel>
+        <Select
+          sx={{ color: colors.darkBlue }}
+          native
+          value={lastName + ' ' + firstName}
+          // @ts-ignore Typings are not considering `native`
+          onChange={handleChangeMultiple}
+          label="Employés"
+          labelId="select-employees-label"
+          id="select-employee"
+          /*inputProps={{
+            id: 'select-employees-label',
+          }}*/
+        >
+          <option value="">Selection</option>
+          {employees.map((employee) => (
+            <option
+              key={employee.id}
+              id={employee.id}
+              value={employee.fullName}
+            >
+              {employee.fullName}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
